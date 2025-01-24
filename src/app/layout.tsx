ae03fd35b7f8d1b162938d0a69f6b1c47309ef23
Canberk Varli <canberkvarli@gmail.com>
2025-01-24 12:55:28 -0800

    fix root error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Newsreader } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
