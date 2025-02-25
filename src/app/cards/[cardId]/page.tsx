edd9e075cab3d7a930a610ceb462bbe56b556823
Canberk Varli <canberkvarli@gmail.com>
2025-02-24 18:03:51 -0800

    update ts

--- src/app/cards/[cardId]/page.tsx
+++ src/app/cards/[cardId]/page.tsx
@@ -6,6 +6,7 @@ export default async function CardDetailPage({
   params,
 }: {
   params: { cardId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const { cardId } = params;
   const card = await getCard({ cardId });
