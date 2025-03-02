695968cd2ad44fd1a5701e03eb646756f96c7e29
Canberk Varli <canberkvarli@gmail.com>
2025-03-01 18:44:49 -0800

    another attempt, add runtime nodejs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
-export const revalidate = 0;
+export const runtime = "nodejs";
+export const revalidate = 0; // or dynamic = "force-dynamic"
+
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
